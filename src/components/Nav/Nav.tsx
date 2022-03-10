6574781326919241fff76105131d32c6ebf9bcc0
kamilsulgut <kamilsulgut@gmail.com>
2022-03-10 18:06:59 +0100

    add mobile navigation

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,15 +1,51 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
-import { Navbar, NavbarDesktop, Link } from "../../styles/Nav.styles";
+import {
+  Navbar,
+  NavbarDesktop,
+  Link,
+  NavbarMobile,
+  OpenMobileMenu,
+  MobileLinks,
+  CloseMobileMenu,
+  MobileLink,
+} from "../../styles/Nav.styles";
 
-const Nav = () => {
+const Nav: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const togleMobileMenu = (e: React.SyntheticEvent<EventTarget>) => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  };
+
+  const mobile = (
+    <MobileLinks>
+      <MobileLink to="/home" onClick={togleMobileMenu}>
+        Home
+      </MobileLink>
+      <MobileLink to="/login" onClick={togleMobileMenu}>
+        Login
+      </MobileLink>
+    </MobileLinks>
+  );
   return (
-    <Navbar>
-      <NavbarDesktop>
-        <Link to="/home">Home</Link>
-        <Link to="/login">Login</Link>
-      </NavbarDesktop>
-    </Navbar>
+    <>
+      <Navbar>
+        <NavbarDesktop>
+          <Link to="/home">Home</Link>
+          <Link to="/login">Login</Link>
+        </NavbarDesktop>
+        <NavbarMobile>
+          {isOpen ? (
+            <CloseMobileMenu onClick={togleMobileMenu} />
+          ) : (
+            <OpenMobileMenu onClick={togleMobileMenu} />
+          )}
+        </NavbarMobile>
+      </Navbar>
+      {isOpen ? mobile : null}
+    </>
   );
 };
 
--- src/styles/Nav.styles.ts
+++ src/styles/Nav.styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
+import { HiOutlineMenu } from "react-icons/hi";
+import { IoMdClose } from "react-icons/io";
 
 export const Navbar = styled.nav`
   width: 100vw;
@@ -9,11 +11,69 @@ export const Navbar = styled.nav`
   align-items: center;
   background-color: #011627;
 `;
-export const NavbarDesktop = styled.div``;
+export const NavbarDesktop = styled.div`
+  @media (max-width: 860px) {
+    display: none;
+  }
+`;
 export const Link = styled(NavLink)`
   margin-right: 20px;
   text-transform: uppercase;
-  font-size: large;
+  font-size: 1.2rem;
   text-decoration: none;
   color: #fdfffc;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    color: #2ec4b6;
+  }
+`;
+
+export const NavbarMobile = styled.div`
+  @media (min-width: 860px) {
+    display: none;
+  }
+`;
+
+export const OpenMobileMenu = styled(HiOutlineMenu)`
+  height: 1.3rem;
+  width: 1.3rem;
+  margin-right: 30px;
+  background-color: none;
+  border: none;
+  color: #fdfffc;
+  cursor: pointer;
+`;
+
+export const CloseMobileMenu = styled(IoMdClose)`
+  height: 1.3rem;
+  width: 1.3rem;
+  margin-right: 30px;
+  background-color: none;
+  border: none;
+  color: #fdfffc;
+  cursor: pointer;
+`;
+
+export const MobileLinks = styled.div`
+  height: 50vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: #011627ad;
+`;
+
+export const MobileLink = styled(NavLink)`
+  margin-top: 20px;
+  text-transform: uppercase;
+  font-size: 1.2rem;
+  text-decoration: none;
+  color: #fdfffc;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    color: #2ec4b6;
+  }
 `;
